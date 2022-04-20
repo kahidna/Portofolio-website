73a86b1ce157d0e02eca4faee18e146503d1896d
Your Name <151402040dianawulandari@gmail.com>
2022-04-20 23:28:12 +0700

    first commit

---
+++ data/config.js
@@ -0,0 +1,56 @@
+export const meta = {
+  title: 'Diana Wulandari - Web Developer and Designer',
+  description: 'Hey, I\'m Diana Wulandari. I\'m a web developer and designer enthusiast.'
+}
+
+export const hero = {
+  title: "Hey there! I'm Diana Wulandari.",
+  desc: "I'm a web developer and designer enthusiast."
+};
+
+export const projects = {
+  title: 'My Projects',
+  desc: "Here are some of my favorite projects that I've worked on.",
+  projects: [
+    {
+      title: 'Dashboard',
+      description: 'This is Dashboard for Temperature celcius and graph using react js',
+      github: 'https://github.com/dianawland/dashboard'
+    },
+    {
+      title: 'CRUD laravel',
+      description: 'Make CREATE,READ,UPDATE,DELETE with laravel',
+      github: 'https://github.com/dianawland/belajar_crud_laravel'
+    },
+    {
+      title: 'Daily UI #024 Boarding Pass',
+      description: 'Design a boarding pass. Consider the origin, the destination, gates, seats, the airline,',
+      link: 'https://dribbble.com/shots/18026877-Daily-UI-024-Boarding-Pass'
+    }
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML5',
+    'CSS3',
+    'Jquery',
+    'UI/UX design',
+    'Mysql',
+    'php',
+    'Laravel',
+    'TailwindCSS',
+    'Figma',
+    'react js',
+    'Git'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: '151402040dianawulandari@gmail.com',
+  github: 'https://github.com/dianawland',
+  dribbble: 'https://dribbble.com/dianawuland',
+  linkedin: 'https://www.linkedin.com/feed/'
+};
